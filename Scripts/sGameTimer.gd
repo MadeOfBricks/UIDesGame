197657d163f04420ef2f2de9b44cde017d3a74eb
LBingham <droiba@yahoo.com>
2018-04-01 14:39:10 -0600

    Working on Beau-Lucas merge

---
+++ Scripts/sGameTimer.gd
@@ -0,0 +1,18 @@
+extends Timer
+var count = 3;
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+    connect("timeout",self,"_on_timeout")
+func _on_timeout():
+	if (count >= 1):
+		print(count)
+		get_node("../Label").set_text("%d" % count)
+		count = count-1;
+	elif (count <= 0 and count >= -1):
+		get_node("../Label").set_text("Go!")
+		count = count - 1;
+	else:
+		get_node("../Label").set_text("")
