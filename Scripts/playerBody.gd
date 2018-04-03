709b4afbb768f22a3282bae633835a0ced48bdc6
LBingham <droiba@yahoo.com>
2018-04-02 22:16:50 -0600

    STILL CHANGING SHIT

---
+++ Scripts/playerBody.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+onready var brain = get_node("Brain")
+
+#Brain will tell us when to walk
+func _ready():
+	
+	brain.connect("body_walk",self,"_on_body_walk")
+	
+	
+#When we need to walk, set pos to new vector provided by brain
+func _on_body_walk(vec):
+	move(vec)
+
+func _on_area_enter(other):
+	
