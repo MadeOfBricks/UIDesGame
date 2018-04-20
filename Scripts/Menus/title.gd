be2b33041bc2002f19409eefbcf1e0c2601d7891
MadeOfBricks <droiba@yahoo.com>
2018-04-20 10:00:49 -0600

    Changed some fonts

--- Scripts/Menus/title.gd
+++ Scripts/Menus/title.gd
@@ -21,7 +21,3 @@ func _on_Training_released():
 
 func _on_Options_released():
 	get_tree().change_scene("res://Scenes/optionsScreen.tscn")
-
-
-func _on_Play_pressed():
-	get_node("../Loading").set_text("Loading...")
