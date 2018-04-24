956a39a97d41ddd491781881c6d6cc2cf4acc4f4
wstory21 <wbstory@mavs.coloradomesa.edu>
2018-04-24 17:21:23 -0600

    Training Screen updated, but not yet finished

--- Scripts/Menus/trainingScreen.gd
+++ Scripts/Menus/trainingScreen.gd
@@ -3,12 +3,37 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
+var count = 0;
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	get_node("ColorFrame 3").hide()
+	get_node("Label 3").hide()
+	get_node("ColorFrame 4").hide()
+	get_node("Label 4").hide()
+	get_node("Label").hide()
 
 
 func _on_MenuButton_released():
 	get_tree().change_scene("res://Scenes/titleScreen.tscn")
+
+
+func _on_ColorFrame_5_input_event( ev ):
+	if (ev.type==InputEvent.MOUSE_BUTTON
+    and ev.button_index == BUTTON_LEFT
+    and ev.is_pressed()):
+		if (count == 0):
+			get_node("ColorFrame 3").show()
+			get_node("Label 3").show()
+			get_node("ColorFrame 2").hide()
+			get_node("Label 2").hide()
+			count = 1
+		elif (count == 1):
+			get_node("ColorFrame 4").show()
+			get_node("Label 4").show()
+			get_node("Label").show()
+			get_node("ColorFrame 3").hide()
+			get_node("Label 3").hide()
+			count += 1
+		elif (count > 1):
+			get_node("ColorFrame 4").hide()
+			get_node("Label").hide()
+			get_node("Label 4").hide()
