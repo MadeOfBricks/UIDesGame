8efc35b2840a92fc137fe9fd4eb053be09969ec8
MadeOfBricks <droiba@yahoo.com>
2018-04-26 10:30:11 -0600

    Changed screenspace controller, allow multi-touch

--- Scripts/Menus/controlLeft.gd
+++ Scripts/Menus/controlLeft.gd
@@ -14,29 +14,27 @@ func _ready():
 	set_process_input(true)
 	var X = viewSize.end.x
 	var Y = viewSize.end.y
-	set_size(X,Y)
+	set_size(Vector2(X,Y))
 	
 
 
 func _input(ev):
-	if ev.x < viewSize.end.x:
-		if ev.type == InputEvent.SCREEN_DRAG:
+	if ev.type == InputEvent.SCREEN_DRAG \
+	|| ev.type == InputEvent.SCREEN_TOUCH:
+		if ev.x < viewSize.end.x:
+			if ev.type == InputEvent.SCREEN_DRAG:
+				
+				var vec = ev.relative_pos
+				
+				if abs(vec.length()) >= 3:
+					lastVec = vec
+				else:
+					vec = lastVec
+				
+				emit_signal("walk_input",vec)
 			
-			var vec = ev.relative_pos
-			#debug._set_text(String(vec))
-			#debug._set_text("Length: " + String(vec.length()))
-			
-			if abs(vec.length()) >= 3:
-				lastVec = vec
-			else:
-				#debug._add_line("Last is: " + String(lastVec))
-				vec = lastVec
-			
-			#debug._add_line("Signaling with " + String(vec))
-			emit_signal("walk_input",vec)
-		
-		if ev.type == InputEvent.SCREEN_TOUCH:
-			var vec = lastVec
-			emit_signal("walk_input",vec)
-			#debug._add_line("TOUCH")
+			if ev.type == InputEvent.SCREEN_TOUCH:
+				var vec = lastVec
+				emit_signal("walk_input",vec)
+				#debug._add_line("TOUCH")
 
--- Scripts/Menus/controlRight.gd
+++ Scripts/Menus/controlRight.gd
@@ -2,12 +2,17 @@ extends Control
 
 onready var main = get_parent().get_parent()
 onready var debug = main.get_node("DBText")
+onready var viewSize = main.get_viewport_rect()
 
 signal attack_input
 
 
 func _ready():
 	set_process_input(true)
+	set_process_input(true)
+	var X = viewSize.end.x
+	var Y = viewSize.end.y
+	set_size(Vector2(X,Y))
 	set_pos(Vector2(OS.get_window_size().x/2,50))
 
 func _draw():
